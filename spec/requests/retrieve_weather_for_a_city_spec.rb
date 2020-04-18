c9344b09d317cd69a8e26574b9346a31392ee11e
Cassie Achzenick <caachzenick@gmail.com>
2020-04-18 10:21:13 -0600

    Add test for weather by location endpoint

---
+++ spec/requests/retrieve_weather_for_a_city_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe "Retrieve weather for a city" do
+  it "returns the weather based on city and state input" do
+    get '/api/v1/forecast?location=denver,co'
+
+    expect(response).to be_successful
+
+    json = JSON.parse(response.body)
+
+    expect(merchants["data"].count).to eq(3)
+  end
+end
