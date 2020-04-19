ed85e113fb6ac32a642ccbde08745a0b1622ad52
Cassie Achzenick <caachzenick@gmail.com>
2020-04-18 20:36:55 -0600

    Returns 401 error if user isn't created

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,8 +3,8 @@ class Api::V1::UsersController < ApplicationController
     user = User.new(email: params["email"], password: params["password"], password_confirmation: params["password_confirmation"])
     if user.save
       render json: user.as_json(only: [:id, :email, :authentication_token]), status: :created
-    elsif user&.user.valid_password?(params["password"])
-      render json: user.as_json(only: [:id, :email, :authentication_token]), status: :created
+    # elsif user&.valid_password?(params["password"])
+    #   render json: user.as_json(only: [:id, :email, :authentication_token]), status: :created
     else
       head(:unauthorized)
     end
--- spec/requests/user_is_created_spec.rb
+++ spec/requests/user_is_created_spec.rb
@@ -16,6 +16,14 @@ describe "A post request to /api/v1/users" do
   end
 
   it "doesn't create a new user" do
+    expect(User.all.length).to eq(0)
 
+    post '/api/v1/users', :params => {email: "whatever@example.com",
+                                      password: "password",
+                                      password_confirmation: "passrrword"}
+
+    expect(response.response_code).to eq 401
+
+    expect(User.all.length).to eq(0)
   end
 end
