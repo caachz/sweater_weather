00e7d466460c2ebf0b51c24a9cba47a791b523ea
Cassie Achzenick <caachzenick@gmail.com>
2020-04-18 20:33:28 -0600

    User can be created

--- Gemfile
+++ Gemfile
@@ -34,7 +34,7 @@ gem 'faraday'
 
 gem 'devise'
 
-gem ''
+gem 'simple_token_authentication', '~> 1.0'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,10 +1,10 @@
 class Api::V1::UsersController < ApplicationController
   def create
-    require "pry"; binding.pry
-    user = User.where(email: params["email"]).first
-
-    if user.valide_password?(params["password"])
-      render json: user.as_json(only: [:id, :email]), status: :created
+    user = User.new(email: params["email"], password: params["password"], password_confirmation: params["password_confirmation"])
+    if user.save
+      render json: user.as_json(only: [:id, :email, :authentication_token]), status: :created
+    elsif user&.user.valid_password?(params["password"])
+      render json: user.as_json(only: [:id, :email, :authentication_token]), status: :created
     else
       head(:unauthorized)
     end
