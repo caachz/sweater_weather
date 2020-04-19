aebca0211000b85af0d27ae3e3b7639cede5dc9f
Cassie Achzenick <caachzenick@gmail.com>
2020-04-19 10:43:10 -0600

    User is passed json

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,10 +1,11 @@
 class Api::V1::UsersController < ApplicationController
   def create
-    user = User.new(email: params["email"], password: params["password"], password_confirmation: params["password_confirmation"])
+    user_info = JSON.parse(params.keys.first, symbolize_names: true)
+
+    user = User.new(email: user_info[:email], password: user_info[:password], password_confirmation: user_info[:password_confirmation])
+
     if user.save
       render json: user.as_json(only: [:id, :email, :authentication_token]), status: :created
-    # elsif user&.valid_password?(params["password"])
-    #   render json: user.as_json(only: [:id, :email, :authentication_token]), status: :created
     else
       head(:unauthorized)
     end
--- spec/requests/user_is_created_spec.rb
+++ spec/requests/user_is_created_spec.rb
@@ -1,12 +1,13 @@
 require 'rails_helper'
+require 'json'
 
 describe "A post request to /api/v1/users" do
   it "creates a new user" do
     expect(User.all.length).to eq(0)
 
-    post '/api/v1/users', :params => {email: "whatever@example.com",
-                                      password: "password",
-                                      password_confirmation: "password"}
+    params = {email: "whatever@example.com", password: "password", password_confirmation: "password"}
+
+    post '/api/v1/users', params: params.to_json
 
     expect(response).to be_successful
 
@@ -16,11 +17,12 @@ describe "A post request to /api/v1/users" do
   end
 
   it "doesn't create a new user" do
+
     expect(User.all.length).to eq(0)
 
-    post '/api/v1/users', :params => {email: "whatever@example.com",
-                                      password: "password",
-                                      password_confirmation: "passrrword"}
+    params = {email: "whatever@example.com", password: "password", password_confirmation: "passrrword"}
+
+    post '/api/v1/users', params: params.to_json
 
     expect(response.response_code).to eq 401
 
