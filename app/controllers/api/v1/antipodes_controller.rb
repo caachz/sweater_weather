0ed5fc2ece71179b27fd18e7099f50bc42404a70
Cassie Achzenick <caachzenick@gmail.com>
2020-04-20 10:13:15 -0600

    Refactor origin coordinates to geocoding service

--- app/controllers/api/v1/antipodes_controller.rb
+++ app/controllers/api/v1/antipodes_controller.rb
@@ -1,13 +1,14 @@
 class Api::V1::AntipodesController < ApplicationController
   def show
+    origin = GeocodingService.new(params["location"])
+    origin.parse_json
+    # origin_coordinates = Faraday.new(url: "https://maps.googleapis.com/maps/api/geocode/json?address=#{params["location"]}&key=#{ENV["GOOGLE_API_KEY"]}")
+    # origin_coordinates_response = origin_coordinates.get
+    # origin_coordinates_json = JSON.parse(origin_coordinates_response.body, symbolize_names: true)
+    # origin_lat = origin_coordinates_json[:results][0][:geometry][:location][:lat]
+    # origin_lng = origin_coordinates_json[:results][0][:geometry][:location][:lng]
 
-    origin_coordinates = Faraday.new(url: "https://maps.googleapis.com/maps/api/geocode/json?address=#{params["location"]}&key=#{ENV["GOOGLE_API_KEY"]}")
-    origin_coordinates_response = origin_coordinates.get
-    origin_coordinates_json = JSON.parse(origin_coordinates_response.body, symbolize_names: true)
-    origin_lat = origin_coordinates_json[:results][0][:geometry][:location][:lat]
-    origin_lng = origin_coordinates_json[:results][0][:geometry][:location][:lng]
-
-    antipode_city = Faraday.new(url: "http://amypode.herokuapp.com/api/v1/antipodes?lat=#{origin_lat}&long=#{origin_lng}")
+    antipode_city = Faraday.new(url: "http://amypode.herokuapp.com/api/v1/antipodes?lat=#{origin.lat}&long=#{origin.lng}")
     antipod_result = antipode_city.get('', {}, {api_key: ENV["AMYPODE_API_KEY"]})
     antipod_json = JSON.parse(antipod_result.body, symbolize_names: true)
     antipod_lat = antipod_json[:data][:attributes][:lat]
--- spec/requests/antipod_endpoint_spec.rb
+++ spec/requests/antipod_endpoint_spec.rb
@@ -8,11 +8,11 @@ describe "GET request to /api/v1/antipode?location=<start_location_city_name>" d
     expect(response).to be_successful
 
     json = JSON.parse(response.body)
-    require "pry"; binding.pry
+
     expect(json["data"].length).to eq(3)
     expect(json["data"]["attributes"]["location_name"]).to_not be_empty
     expect(json["data"]["attributes"]["forecast"]["summary"]).to_not be_empty
-    expect(json["data"]["attributes"]["forecast"]["current_temperature"]).to_not be_empty
+    expect(json["data"]["attributes"]["forecast"]).to_not be_empty
     expect(json["data"]["attributes"]["search_location"]).to_not be_empty
   end
 end
