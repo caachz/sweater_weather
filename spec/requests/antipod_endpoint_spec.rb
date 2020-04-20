eeef76b361ac9afbde4a110a37fe55354da82af0
Cassie Achzenick <caachzenick@gmail.com>
2020-04-20 10:07:25 -0600

    Add passing test

--- spec/requests/antipod_endpoint_spec.rb
+++ spec/requests/antipod_endpoint_spec.rb
@@ -9,9 +9,10 @@ describe "GET request to /api/v1/antipode?location=<start_location_city_name>" d
 
     json = JSON.parse(response.body)
     require "pry"; binding.pry
-    # expect(json["data"].length).to eq(3)
-    # expect(json["data"]["attributes"]["today"].length).to eq(10)
-    # expect(json["data"]["attributes"]["hourly"].length).to eq(8)
-    # expect(json["data"]["attributes"]["daily"].length).to eq(5)
+    expect(json["data"].length).to eq(3)
+    expect(json["data"]["attributes"]["location_name"]).to_not be_empty
+    expect(json["data"]["attributes"]["forecast"]["summary"]).to_not be_empty
+    expect(json["data"]["attributes"]["forecast"]["current_temperature"]).to_not be_empty
+    expect(json["data"]["attributes"]["search_location"]).to_not be_empty
   end
 end
