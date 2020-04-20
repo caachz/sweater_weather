66879b20035be303cd8c78663b1ddc3387dcfebe
Cassie Achzenick <caachzenick@gmail.com>
2020-04-20 16:46:42 -0600

    Simplify controller

--- app/controllers/api/v1/antipodes_controller.rb
+++ app/controllers/api/v1/antipodes_controller.rb
@@ -3,19 +3,5 @@ class Api::V1::AntipodesController < ApplicationController
     info = AntipodeFacade.new(params["location"])
 
     render json: AntipodeSerializer.new(info)
-#     render json: {
-#     "data": {
-#         "id": "null",
-#         "type": "antipode",
-#         "attributes": {
-#             "location_name": info.antipode_name.city_name,
-#             "forecast": {
-#                 "summary": info.antipode_weather.forecast.today[:today_description],
-#                 "current_temperature": info.antipode_weather.forecast.today[:temp_now],
-#             },
-#             "search_location": params["location"]
-#         }
-#     }
-# }
   end
 end
